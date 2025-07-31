d950d3bb2c03439ed3d2dbefd4544fbf12248756
Juan José Cortés Rodríguez <juan.jose.cortes@correounivalle.edu.co>
2025-07-30 23:44:16 -0500

    some fixes 2

--- backend/apps/publicity/serializer.py
+++ backend/apps/publicity/serializer.py
@@ -13,12 +13,3 @@ class PublicitySerializer(serializers.ModelSerializer):
             return obj.image.url
         return None
 
-    #prueba, no se si esta bien jahsjshbajbhadjsahdja
-    # def ejecutar_por_fechas(self, data):
-    #     if data.get('is_active') == True:
-    #         diferencia = data.get('start_date') - data.get('end_date')
-    #         if diferencia < 0:
-    #             data.get('is_active') = False
-    #             raise serializers.ValidationError("La fecha de inicio no puede ser posterior a la fecha final.")
-    #     raise serializers.ValidationError("La publicidad debe estar activa para ejecutar por fechas.")
-
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,10 @@
 import axios from "axios"
 
+// VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/";
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
