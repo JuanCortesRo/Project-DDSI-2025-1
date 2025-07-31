a3abaf6c3475a35f6368f374d3c1283d6dead0f8
Juan José Cortés Rodríguez <juan.jose.cortes@correounivalle.edu.co>
2025-07-31 00:15:28 -0500

    some fixes 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/', // ← Esto es importante para que el deploy funcione
   plugins: [react()],
 })
